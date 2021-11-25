7a081a06d485e85f45bf473634f521aab9562d68
Chelpot <cheltest123@gmail.com>
2021-11-25 22:34:03 +0100

    forgot to rename files name from menuQuizSite to menuQuiz

--- menuQuiz/menuQuiz/asgi.py
+++ menuQuiz/menuQuiz/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'menuQuizSite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'menuQuiz.settings')
 
 application = get_asgi_application()
