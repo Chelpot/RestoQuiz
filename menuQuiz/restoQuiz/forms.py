9005480af7a7b8d78ba98d8355d3b3d62647bad2
Chelpot <cheltest123@gmail.com>
2021-12-13 13:53:26 +0100

    fix

--- menuQuiz/restoQuiz/forms.py
+++ menuQuiz/restoQuiz/forms.py
@@ -8,4 +8,4 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('nom', 'email', 'password1', 'password2', )
+        fields = ('name', 'email', 'password1', 'password2', )
